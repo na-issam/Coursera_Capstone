07362df0c4708055e82511ef6e1e8961de2f4a95
na-issam <59018085+na-issam@users.noreply.github.com>
2020-01-04 13:08:54 +0100

    Add files via upload

---
+++ capstone ibm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for ibm capstone project
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[ ]:
+
+
+print('Hello Capstone Project Course!')
+
